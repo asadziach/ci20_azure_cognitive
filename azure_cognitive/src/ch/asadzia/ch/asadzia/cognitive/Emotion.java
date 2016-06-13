c22490deafdb4c9191554123a88fb508d3234227
Asad Zia <asadzia@gmail.com>
2016-06-13 19:06:00 +0500

    Emotion API working with online url

---
+++ azure_cognitive/src/ch/asadzia/ch/asadzia/cognitive/Emotion.java
@@ -0,0 +1,56 @@
+package ch.asadzia.ch.asadzia.cognitive;
+
+// // This sample uses the Apache HTTP client from HTTP Components (http://hc.apache.org/httpcomponents-client-ga/)
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * Created by asad on 6/13/16.
+ */
+public class Emotion {
+    public static void main(String[] args)
+    {
+        String apikey = System.getenv("EMOTION_KEY");
+
+        if(apikey==null)
+            throw new IllegalArgumentException("Please define environment variable EMOTION_KEY");
+
+        HttpClient httpclient = HttpClients.createDefault();
+
+        try
+        {
+            URIBuilder builder = new URIBuilder("https://api.projectoxford.ai/emotion/v1.0/recognize");
+
+
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", apikey);
+
+
+            // Request body
+            StringEntity reqEntity = new StringEntity("{ \"url\": \"https://portalstoragewuprod.azureedge.net/emotion/recognition3.jpg\" }");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                System.out.println(EntityUtils.toString(entity));
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+}
